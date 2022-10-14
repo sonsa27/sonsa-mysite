970d3b44b02f509e76254388cdcce2316d4b39bd
sonsa <songsa.c2007@gmail.com>
2022-10-14 10:28:49 +0900

    エラー修正

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from http.client import HTTPResponse
 from django.http import HttpResponse
 
 def index(request):
-    return HTTPResponse("hello world")
+    return HttpResponse("hello world")
 
 def detail(request):
     return HttpResponse("detail page")
