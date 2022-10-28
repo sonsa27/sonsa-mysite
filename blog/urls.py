586e628c2130e58328d77a7331bda20629a92fe6
sonsa <songsa.c2007@gmail.com>
2022-10-28 12:20:23 +0900

    ブログDB作成

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path("register", views.AccountCreateView.as_view(), name="register"),
     path("login", views.AccountLoginView.as_view(), name="login"),
     path("mypage", views.MypageView.as_view(), name="mypage"),
-    path("logout", views.AccountLogoutView.as_view(), name="logout")
+    path("logout", views.AccountLogoutView.as_view(), name="logout"),
+    path("mypage/new-article", views.ArticleCreateView.as_view(),
+         name="mypage-new-article"),
+    path("mypage/articles", views.MypageArticleView.as_view(),
+         name="mypage-articles"),
 ]
