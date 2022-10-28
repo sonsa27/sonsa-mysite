20ee7182119f9654ff68873bf220186ed74fef22
sonsa <songsa.c2007@gmail.com>
2022-10-28 10:41:11 +0900

    sql復活

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # views.py の index() 関数を呼び出す
     path('', views.index, name='index'),
     path('detail', views.detail, name='detail'),
-    path("register", views.AccountCreateView.as_view(), name="register")
+    path("register", views.AccountCreateView.as_view(), name="register"),
+    path("login", views.AccountLoginView.as_view(), name="login"),
+    path("mypage", views.MypageView.as_view(), name="mypage")
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -27,3 +29,20 @@ class AccountCreateView(View):
         # 登録完了画面を表示する
         return render(request, "blog/register_success.html")
 # 登録完了画面を表示する
+
+
+class AccountLoginView(LoginView):
+    """ログインページのテンプレート"""
+    template_name = 'blog/login.html'
+
+    def get_default_redirect_url(self):
+        """ログインに成功した時に飛ばされるURL"""
+        return "/blog"
+
+
+class MypageView(LoginRequiredMixin, View):
+    # ログインしてない場合ここに飛ばす
+    login_url = '/blog/login'
+
+    def get(self, request):
+        return render(request, "blog/mypage.html")
