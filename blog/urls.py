51a60c9268b3752cfe7f8dc81e4087d761a7dc6e
sonsa <songsa.c2007@gmail.com>
2022-10-28 11:31:56 +0900

    ログアウト追加

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('detail', views.detail, name='detail'),
     path("register", views.AccountCreateView.as_view(), name="register"),
     path("login", views.AccountLoginView.as_view(), name="login"),
-    path("mypage", views.MypageView.as_view(), name="mypage")
+    path("mypage", views.MypageView.as_view(), name="mypage"),
+    path("logout", views.AccountLogoutView.as_view(), name="logout")
 ]
